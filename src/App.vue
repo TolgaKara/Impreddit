d3d750fa701f40ee50c5fe56d999c1bb9371fe24
Tolga Kara <tolgakara2.0@gmail.com>
2020-11-21 18:39:09 +0100

    Added the Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <router-view/>
+
+  <nav>
+    <div class="nav-wrapper deep-purple darken-2">
+      <div class="container">
+        <a href="#" class="brand-logo">Impreddit</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <!-- <li><a href="sass.html">Sass</a></li> -->
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <router-view class="container"/>
 </template>
 
 <style lang="scss">
